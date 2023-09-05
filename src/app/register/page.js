0c813ec20bb2c92b5589226165cac5ee8572ea8a
merndevs520 <merndevs3@gmail.com>
2023-09-05 22:03:54 +0600

    add

---
+++ src/app/register/page.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { MdAccountBox, MdOutlineAccountCircle } from "react-icons/md";
+import { AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div className="h-screen m-5 space-y-3 flex flex-col justify-start  items-center">
+      <p className="text-2xl font-semibold p-2    cursor-pointer hover:bg-slate-600 w-fit duration-200 rounded-md ">
+        Rgister page
+      </p>
+
+      <div className="bg-slate-200 w-4/12  p-10 rounded-2xl ">
+        <div className="w-20 h-20 rounded-full bg-slate-400 mx-auto flex items-center justify-center">
+          <MdOutlineAccountCircle className="rounded-full text-7xl" />
+        </div>
+        <div className="text-gray-800 font-semibold text-xl py-4 space-y-3">
+          <p>Username --</p>
+          <input type="text" className="w-full p-1 rounded-md " />
+          <p>Eamil --</p>
+          <input type="text" className="w-full p-1 rounded-md " />
+          <p>Password --</p>
+          <input type="password" className="w-full p-1 rounded-md " />
+        </div>
+        <Link href="/">
+          <p className="bg-indigo-500 text-center rounded-md px-2 py-1 font-semibold cursor-pointer">
+            Register
+          </p>
+        </Link>
+        <p className="bg-slate-800  my-3 text-center rounded-md px-2 py-1 font-semibold cursor-pointer flex items-center justify-center gap-3">
+          <AiFillGithub className="h-6 w-6" /> Register with Github
+        </p>
+        <Link href="/login">
+          <p className="text-indigo-400 font-semibold text-center  cursor-pointer">
+            Have an account ?
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+// config/mongoose.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectDB;
